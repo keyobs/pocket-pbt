95c435190f1739413c9783b6c030430ae01dae4c
keyobs <elodiekoob@gmail.com>
2024-11-27 17:09:07 +0100

    feat(counter): handle buttons avaibility to match user practices

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -3,6 +3,7 @@ import "./counter.less";
 import { useTimeContext } from "../allCountersActions/useTimeContext";
 
 const Counter = () => {
+  const [wasCountStarted, setWasCountStarted] = useState(false);
   const [count, setCount] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
 
@@ -15,6 +16,11 @@ const Counter = () => {
     }
   }, [count, isPaused, isTimePaused]);
 
+  useEffect(() => {
+    if (count > 0 && !wasCountStarted) setWasCountStarted(true);
+    else if (count === 0 && wasCountStarted) setWasCountStarted(false);
+  }, [count, wasCountStarted]);
+
   const onReset = () => {
     setCount(0);
     setIsPaused(false);
@@ -24,20 +30,29 @@ const Counter = () => {
     setCount(count + 30);
   };
 
+  const startButtonLabel = wasCountStarted
+    ? isPaused
+      ? "resume"
+      : "pause"
+    : "start";
+
+  const onStartButtonClick = () =>
+    wasCountStarted ? setIsPaused(!isPaused) : setCount(30);
+
   return (
     <div className="counter">
       <div className="time">{count}</div>
 
-      <button disabled={count !== 0} onClick={() => setCount(30)}>
-        start
+      <button disabled={isTimePaused!} onClick={() => onStartButtonClick()}>
+        {startButtonLabel}
       </button>
-      <button disabled={count === 0} onClick={() => setIsPaused(!isPaused)}>
-        {isPaused ? "resume" : "pause"}
-      </button>
-      <button disabled={count === 0} onClick={() => onReset()}>
+      <button disabled={count === 0 || !isPaused} onClick={() => onReset()}>
         reset
       </button>
-      <button disabled={count === 0} onClick={() => onAddTime()}>
+      <button
+        disabled={count === 0 && !isTimePaused}
+        onClick={() => onAddTime()}
+      >
         +30
       </button>
     </div>

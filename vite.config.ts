1ddb0887fcb9895154221a11ac5f8f32a0ca1623
keyobs <elodiekoob@gmail.com>
2025-09-05 09:50:32 +0200

    refactor(imports):use alias paths in less files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        paths: [path.resolve(__dirname, "src")],
+      },
+    },
+  },
 });
